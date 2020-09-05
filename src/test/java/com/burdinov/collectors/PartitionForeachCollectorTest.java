4476fb77f0c7aac2ba585ccdf89587d098b0cfab
leonid.burdinov <leonid.burdinov@feedvisor.com>
2020-09-05 13:39:34 +0300

    using AssertJ's assertions instead JUnit's

--- src/test/java/com/burdinov/collectors/PartitionForeachCollectorTest.java
+++ src/test/java/com/burdinov/collectors/PartitionForeachCollectorTest.java
@@ -4,11 +4,11 @@ package com.burdinov.collectors;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class PartitionForeachCollectorTest {
@@ -16,28 +16,36 @@ public class PartitionForeachCollectorTest {
     @Test
     public void all_partitions_equal() {
         List<List<Integer>> result = new ArrayList<>();
-        IntStream.rangeClosed(1, 30).boxed().collect(Collectors.partitionForeach(3, result::add));
-        assertTrue(result.stream().allMatch(subList -> subList.size() == 3));
+        IntStream.rangeClosed(1, 9).boxed().collect(Collectors.partitionForeach(3, result::add));
+        assertThat(result).allSatisfy(sublist -> assertThat(sublist).hasSize(3));
     }
 
     @Test
     public void last_partition_smaller() {
         List<List<Integer>> result = new ArrayList<>();
-        IntStream.range(1, 30).boxed().collect(Collectors.partitionForeach(3, result::add));
-        assertTrue(result.stream().limit(9).allMatch(subList -> subList.size() == 3)); //[[1,2,3], [4,5,6], ..., [25,26,27]]
-        assertEquals(2, result.get(result.size() - 1).size()); // [28, 29]
+        IntStream.range(1, 9).boxed().collect(Collectors.partitionForeach(3, result::add));
+        List<List<Integer>> expected = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(7, 8)
+        );
+        assertThat(result).containsOnlyOnceElementsOf(expected);
     }
 
     @Test
     public void merge_partitions_when_parallel() {
         Object lock = new Object();
         List<List<Integer>> result = new ArrayList<>();
-        IntStream.range(1, 30).boxed().parallel().collect(Collectors.partitionForeach(3, t -> {
+        IntStream.range(1, 9).boxed().parallel().collect(Collectors.partitionForeach(3, t -> {
             synchronized (lock) {
                 result.add(t);
             }
         }));
-        assertTrue(result.stream().limit(9).allMatch(subList -> subList.size() == 3));
-        assertEquals(2, result.get(result.size() - 1).size());
+        List<List<Integer>> expected = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(5, 6, 7),
+                Arrays.asList(4, 8)
+        );
+        assertThat(result).containsOnlyOnceElementsOf(expected);
     }
 }
--- src/test/java/com/burdinov/collectors/PartitioningCollectorTest.java
+++ src/test/java/com/burdinov/collectors/PartitioningCollectorTest.java
@@ -3,32 +3,40 @@ package com.burdinov.collectors;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class PartitioningCollectorTest {
 
     @Test
     public void all_partitions_equal() {
-        List<List<Integer>> collected = IntStream.rangeClosed(1, 30).boxed().collect(Collectors.toPartitionedList(3));
-        assertTrue(collected.stream().allMatch(subList -> subList.size() == 3));
+        List<List<Integer>> collected = IntStream.rangeClosed(1, 9).boxed().collect(Collectors.toPartitionedList(3));
+        assertThat(collected).allSatisfy(sublist -> assertThat(sublist).hasSize(3));
     }
 
     @Test
     public void last_partition_smaller() {
-        List<List<Integer>> collected = IntStream.range(1, 30).boxed().collect(Collectors.toPartitionedList(3));
-        assertTrue(collected.stream().limit(9).allMatch(subList -> subList.size() == 3)); //[[1,2,3], [4,5,6], ..., [25,26,27]]
-        assertEquals(2, collected.get(collected.size() - 1).size()); // [28, 29]
+        List<List<Integer>> collected = IntStream.range(1, 9).boxed().collect(Collectors.toPartitionedList(3));
+        List<List<Integer>> expected = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(4, 5, 6),
+                Arrays.asList(7, 8)
+        );
+        assertThat(collected).containsOnlyOnceElementsOf(expected);
     }
 
     @Test
     public void merge_partitions_when_parallel() {
-        List<List<Integer>> collected = IntStream.range(1, 30).boxed().parallel().collect(Collectors.toPartitionedList(3));
-        assertTrue(collected.stream().limit(9).allMatch(subList -> subList.size() == 3));
-        assertEquals(2, collected.get(collected.size() - 1).size());
+        List<List<Integer>> collected = IntStream.range(1, 9).boxed().parallel().collect(Collectors.toPartitionedList(3));
+        List<List<Integer>> expected = Arrays.asList(
+                Arrays.asList(1, 2, 3),
+                Arrays.asList(5, 6, 7),
+                Arrays.asList(4, 8)
+        );
+        assertThat(collected).containsOnlyOnceElementsOf(expected);
     }
 }
